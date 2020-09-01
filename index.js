729da59bbc7e0d2678e13feb485c93e3aa326505
mgreif <m.greif@autohaus-royal.de>
2020-09-01 13:30:58 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8')
+
+const httpServer = require('http').createServer((req, res) => {
+  res.setHeader('Content-Length', Buffer.byteLength(content))
+  res.end(content)
+})
+
+const io = require('socket.io')(httpServer)
+
+io.on('connect', socket => {
+  socket.broadcast.emit('newUser')
+})
+
+let rooms = {}
+const users = []
+
+const removeEntryOfObjectProperties = (obj, entry) => {
+  const props = Object.entries(obj).map(x => ([x[0], x[1].filter(l => l !== entry)])).reduce((acc, curr) => {
+    acc[curr[0]] = curr[1]
+    return acc
+  }, {})
+  return props
+}
+
+io.on('connection', socket => {
+  socket.on('joinRoom', data => {
+    const { cookie, room, currentRoom } = data
+    if (!users.includes(cookie)) {
+      users.push(cookie)
+    }
+
+    if (currentRoom) {
+      socket.leave(currentRoom)
+      socket.in(currentRoom).emit('user-leave', { ...data, room_left: currentRoom })
+      rooms[currentRoom].splice(rooms[currentRoom].indexOf(cookie), 1)
+    }
+
+    socket.join(room)
+    socket.in(room).emit('user-join', data)
+    rooms = removeEntryOfObjectProperties(rooms, cookie)
+
+    if (!rooms[room]) {
+      rooms[room] = [cookie]
+    } else {
+      if (!rooms[room].includes(cookie)) {
+        rooms[room].push(cookie)
+      }
+    }
+
+    console.log('rooms', rooms)
+    io.emit('update-rooms', rooms)
+  })
+
+  socket.on('send', data => {
+    console.log(data)
+    socket.in(data.currentRoom).emit('newItem', { value: data.value, username: data.username || data.cookie })
+  })
+})
+
+httpServer.listen(3000, () => {
+  console.log('listening on port 3000 ...')
+})
